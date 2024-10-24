dc4fcce1c535439703d35083b519b6288b44336f
Karyne Lorrany <karyne.santana@estudante.ifb.edu.br>
2024-10-24 00:07:26 -0300

    Criação das telas dos Personagens

---
+++ lib/Personagens/betinho.dart
@@ -0,0 +1,205 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:glicogotas_app/Personagens/pumps.dart';
+import 'package:glicogotas_app/home.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class PersonagemBetinhoPage extends StatelessWidget {
+  const PersonagemBetinhoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      backgroundColor: const Color(0xFFEAF7FF),
+      body: Stack(
+        children: [
+          // Fundo com as listras
+          Positioned.fill(
+            child: SvgPicture.asset(
+              'assets/images/fundo-betinho.svg',
+              fit: BoxFit.cover,
+            ),
+          ),
+
+          // Botão de voltar no topo esquerdo
+          Positioned(
+            top: 40,
+            left: 16,
+            child: IconButton(
+              iconSize: 30,
+              icon: const Icon(
+                Icons.home_rounded,
+                color: Color.fromARGB(255, 0, 132, 255),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const TelaHome()),
+                );
+              },
+            ),
+          ),
+
+          // Botão de configurações no topo direito
+          Positioned(
+            top: 40,
+            right: 16,
+            child: IconButton(
+              iconSize: 30,
+              icon: const Icon(
+                Icons.settings,
+                color: Color.fromARGB(255, 0, 132, 255),
+              ),
+              onPressed: () {},
+            ),
+          ),
+
+          // Nome do personagem (Betinho) centralizado
+          Positioned(
+            top: size.height * 0.15,
+            left: 0,
+            right: 0,
+            child: Stack(
+              alignment: Alignment.center, // Alinha os textos exatamente
+              children: [
+                // Texto branco (borda)
+                Text(
+                  'Betinho',
+                  style: GoogleFonts.chewy(
+                    fontSize: size.width * 0.13,
+                    foreground: Paint()
+                      ..style = PaintingStyle.stroke
+                      ..strokeWidth = 8
+                      ..color = const Color(0xFFFFFEFF), // Cor da borda branca
+                    shadows: [
+                      Shadow(
+                        color: Colors.black.withOpacity(0.25), // Sombra suave
+                        offset: const Offset(3.0, 3.0),
+                        blurRadius: 5.0,
+                      ),
+                    ],
+                  ),
+                ),
+                // Texto verde
+                Text(
+                  'Betinho',
+                  style: GoogleFonts.chewy(
+                    fontSize: size.width * 0.13,
+                    color: const Color(0xFF01C881), // Cor verde
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          // A bola do personagem no fundo
+          Positioned(
+            top: size.height * 0.28,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: SvgPicture.asset(
+                'assets/images/eclipse-betinho.svg',
+                height: size.height * 0.36,
+              ),
+            ),
+          ),
+
+          // Personagem sobreposto à bola
+          Positioned(
+            top: size.height * 0.28,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: SvgPicture.asset(
+                'assets/images/betinho-person.svg',
+                height: size.height * 0.38,
+              ),
+            ),
+          ),
+
+          // Descrição do personagem
+          Positioned(
+            bottom: size.height * 0.20,
+            left: 20,
+            right: 20,
+            child: Stack(
+              alignment: Alignment
+                  .center, // Alinhamento central para as camadas de texto
+              children: [
+                // Texto branco (borda)
+                Text(
+                  'O monitor de glicemia, sempre atento para manter a Lita segura!',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.chewy(
+                    fontSize: size.width * 0.05,
+                    foreground: Paint()
+                      ..style = PaintingStyle.stroke
+                      ..strokeWidth = 8
+                      ..color = const Color(0xFFFFFEFF), // Cor da borda branca
+                    shadows: [
+                      Shadow(
+                        color: Colors.black.withOpacity(0.25), // Sombra suave
+                        offset: const Offset(3.0, 3.0),
+                        blurRadius: 5.0,
+                      ),
+                    ],
+                  ),
+                ),
+                // Texto verde
+                Text(
+                  'O monitor de glicemia, sempre atento para manter a Lita segura!',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.chewy(
+                    fontSize: size.width * 0.05,
+                    color: const Color(0xFF01C881), // Cor verde
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          // Botões de navegação e som
+          Positioned(
+            bottom:
+                22, // Ajuste a altura aqui para aumentar a posição dos botões
+            left: 20,
+            child: IconButton(
+              icon: SvgPicture.asset(
+                'assets/images/btn-voltar-azul.svg',
+                width: 65,
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const PersonagemPumpsPage()),
+                ); // Ação do botão voltar
+              },
+            ),
+          ),
+
+          // Centralizando o botão do meio usando Align
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  bottom: 22), // Ajuste a altura aqui também
+              child: IconButton(
+                icon: SvgPicture.asset(
+                  'assets/images/btn-som-azul.svg',
+                  width: 65,
+                ),
+                onPressed: () {
+                  // Ação do botão som
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
