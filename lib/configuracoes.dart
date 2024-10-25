a9c9891b0b2502edb7d28f17d554c105dd6fa3c2
Jordana Barbosa <jordananabarbosa@hotmail.com>
2024-10-24 21:44:54 -0300

    Configurações, botão de parar rosa

--- lib/configuracoes.dart
+++ lib/configuracoes.dart
@@ -5,27 +5,26 @@ class ConfigDialog extends StatefulWidget {
   const ConfigDialog({super.key});
 
   @override
-  ConfigDialogState createState() =>
-      ConfigDialogState(); // Classe agora pública
+  ConfigDialogState createState() => ConfigDialogState();
 }
 
 class ConfigDialogState extends State<ConfigDialog> {
-  double _somValue = 0.5;
-  double _musicaValue = 0.5;
+  bool _somOn = true; // Estado do switch Som (on/off)
+  bool _musicaOn = true; // Estado do switch Música (on/off)
   double _volumeValue = 0.7;
-  String _selectedLanguage = 'Português'; // Armazena o idioma selecionado
+  String _selectedLanguage = 'Português';
 
   @override
   Widget build(BuildContext context) {
     return Dialog(
-      backgroundColor: Colors.transparent, // Fundo transparente
+      backgroundColor: Colors.transparent,
       insetPadding: const EdgeInsets.all(10),
       child: Center(
         child: Container(
-          width: 320,
+          width: 300,
           padding: const EdgeInsets.all(16.0),
           decoration: BoxDecoration(
-            color: Colors.white,
+            color: const Color(0xFF008AD7),
             borderRadius: BorderRadius.circular(20),
             boxShadow: [
               BoxShadow(
@@ -38,84 +37,128 @@ class ConfigDialogState extends State<ConfigDialog> {
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              // Linha com o ícone de voltar, título, e ícone de configuração
+              // "Configurações" e voltar
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   IconButton(
                     icon:
-                        const Icon(Icons.arrow_back, color: Color(0xFF37ABDC)),
+                        const Icon(Icons.arrow_back, color: Color(0xFFFEDE74)),
                     onPressed: () {
-                      Navigator.of(context)
-                          .pop(); // Fecha o diálogo e volta à tela anterior
+                      Navigator.of(context).pop();
                     },
                   ),
-                  Text(
-                    'CONFIGURAÇÕES',
-                    style: GoogleFonts.podkova(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold, // Fonte BOLD
-                      color: const Color(0xFF37ABDC),
+                  Expanded(
+                    child: Center(
+                      child: Text(
+                        'CONFIGURAÇÕES',
+                        style: GoogleFonts.chewy(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: const Color(0xFFFCB44E), // Cor FCB44E
+                          shadows: [
+                            Shadow(
+                              color: Colors.black.withOpacity(0.5),
+                              offset: const Offset(2, 2),
+                              blurRadius: 4,
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
                   ),
-                  Icon(Icons.settings,
-                      color:
-                          Color(0xFFFCB44E)), // Ícone decorativo da engrenagem
+                  const SizedBox(
+                      width:
+                          48), // Espaço vazio para balancear o ícone de voltar
                 ],
               ),
-              const SizedBox(height: 30), // Aumentado para dar mais espaço
+              const SizedBox(height: 20),
 
-              // Slider para som
-              _buildSliderOption('SOM', _somValue, (newValue) {
+              // Ajuste de som com Switch on/off
+              _buildSwitchOption('SOM', _somOn, (value) {
                 setState(() {
-                  _somValue = newValue;
+                  _somOn = value;
                 });
               }),
 
-              const SizedBox(
-                  height: 10), // Espaçamento adicional entre os sliders
+              const SizedBox(height: 20), // Espaçamento ajustado
 
-              // Slider para música
-              _buildSliderOption('MÚSICA', _musicaValue, (newValue) {
+              // Ajuste de música com Switch on/off
+              _buildSwitchOption('MÚSICA', _musicaOn, (value) {
                 setState(() {
-                  _musicaValue = newValue;
+                  _musicaOn = value;
                 });
               }),
 
-              const SizedBox(
-                  height: 10), // Espaçamento adicional entre os sliders
+              const SizedBox(height: 20), // Espaçamento ajustado
 
-              // Slider para volume
-              _buildSliderOption('VOLUME', _volumeValue, (newValue) {
+              // Ajuste de volume com Slider
+              _buildSliderOption('VOLUME', _volumeValue, (value) {
                 setState(() {
-                  _volumeValue = newValue;
+                  _volumeValue = value;
                 });
               }),
 
               const SizedBox(height: 20),
 
-              // Seção de Idioma com botões horizontais
-              _buildLanguageSelector(),
-
-              const SizedBox(height: 20),
-
-              // Botão de ajuda
-              _buildHelpButton(context),
-
-              const SizedBox(height: 20),
-
-              // Botão para fechar
+              // idioma
               Align(
-                alignment: Alignment.centerRight,
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop(); // Fecha o diálogo
-                  },
-                  child: const Text(
-                    'Fechar',
-                    style: TextStyle(color: Colors.blue),
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'IDIOMA:',
+                  style: GoogleFonts.chewy(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: const Color(0xFFFFFFFF), // Cor FCB44E
+                    shadows: [
+                      Shadow(
+                        color: Colors.black.withOpacity(0.5),
+                        offset: const Offset(2, 2),
+                        blurRadius: 4,
+                      ),
+                    ],
                   ),
                 ),
+              ),
+              const SizedBox(height: 10), // Espaço entre "Idioma" e os botões
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _buildLanguageButton('Português'),
+                  const SizedBox(width: 10),
+                  _buildLanguageButton('Español'),
+                  const SizedBox(width: 10),
+                  _buildLanguageButton('English'),
+                ],
+              ),
+
+              const SizedBox(height: 20),
+
+              //  ícone ?
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'AJUDA',
+                    style: GoogleFonts.podkova(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: const Color(0xFFFFFFFF),
+                      shadows: [
+                        Shadow(
+                          color: Colors.black.withOpacity(0.5),
+                          offset: const Offset(2, 2),
+                          blurRadius: 4,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Icon(
+                    Icons.help_outline,
+                    color: const Color(0xFFFEDE74), // Ícone de ajuda
+                  ),
+                ],
               ),
             ],
           ),
@@ -124,131 +167,111 @@ class ConfigDialogState extends State<ConfigDialog> {
     );
   }
 
-  // Função para criar sliders com cores personalizadas
+  // Função para criar as opções de controle deslizante
   Widget _buildSliderOption(
-      String title, double currentValue, ValueChanged<double> onChanged) {
+      String title, double value, ValueChanged<double> onChanged) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
           title,
-          style: GoogleFonts.podkova(
+          style: GoogleFonts.chewy(
             fontSize: 16,
-            fontWeight: FontWeight.bold, // Fonte BOLD
-            color: const Color(0xFF37ABDC), // Cor azul
+            fontWeight: FontWeight.bold,
+            color: const Color(0xFFFFFFFF), // Títulos em FCB44E
+            shadows: [
+              Shadow(
+                color: Colors.black.withOpacity(0.5),
+                offset: const Offset(2, 2),
+                blurRadius: 4,
+              ),
+            ],
           ),
         ),
-        const SizedBox(height: 10), // Adicionado espaçamento antes do slider
-        Slider(
-          value: currentValue,
-          min: 0,
-          max: 1,
-          divisions: 10,
-          label: (currentValue * 100).toInt().toString(),
-          onChanged: onChanged,
-          activeColor: const Color(0xFFFCB44E), // Cor preenchida (bolinha)
-          inactiveColor: const Color(0xFF37ABDC), // Parte não preenchida
-          thumbColor: const Color(0xFFFCB44E), // Cor da bolinha do slider
+        SliderTheme(
+          data: SliderThemeData(
+            trackHeight: 4, // Altura da barra
+            thumbColor: const Color(0xFFFCB44E), // Cor da bolinha
+            activeTrackColor: const Color(0xFFFCB44E), // Parte preenchida
+            inactiveTrackColor: const Color(0xFFFFFFFF), // Parte não preenchida
+          ),
+          child: Slider(
+            value: value,
+            min: 0,
+            max: 1,
+            onChanged: onChanged,
+          ),
         ),
       ],
     );
   }
 
-  // Seletor de idioma com botões horizontais
-  Widget _buildLanguageSelector() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          'IDIOMA',
-          style: GoogleFonts.podkova(
-            fontSize: 16,
-            fontWeight: FontWeight.bold, // Fonte BOLD
-            color: const Color(0xFF37ABDC), // Cor azul
-          ),
-        ),
-        const SizedBox(height: 8),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            _buildLanguageButton('Português', 'Português'),
-            const SizedBox(width: 10), // Espaçamento entre os botões
-            _buildLanguageButton('Español', 'Espanhol'),
-            const SizedBox(width: 10),
-            _buildLanguageButton('English', 'Inglês'),
-          ],
-        ),
-      ],
-    );
-  }
-
-  // Função para criar botões de idioma personalizados
-  Widget _buildLanguageButton(String language, String value) {
+  // Função para criar os botões de idioma
+  Widget _buildLanguageButton(String language) {
     return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: _selectedLanguage == language
+            ? const Color(0xFFFCB44E) // Quando selecionado
+            : const Color(0xFF37ABDC), // Quando não selecionado
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        shadowColor: Colors.black.withOpacity(0.5), // Sombra dos botões
+        elevation: 5, // Adiciona sombra visível
+      ),
       onPressed: () {
         setState(() {
-          _selectedLanguage = value;
+          _selectedLanguage = language;
         });
       },
-      style: ElevatedButton.styleFrom(
-        backgroundColor:
-            _selectedLanguage == value ? Colors.blue : Colors.grey[300],
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20),
-        ),
-        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-      ),
       child: Text(
         language,
-        style: GoogleFonts.podkova(
-          fontSize: 14, // Reduzindo o tamanho da fonte dos idiomas
+        style: GoogleFonts.chewy(
+          fontSize: 12,
           fontWeight: FontWeight.bold,
-          color: _selectedLanguage == value
-              ? Colors.white
-              : const Color(0xFFFCB44E),
-        ),
-      ),
-    );
-  }
-
-  // Função para o botão de ajuda
-  Widget _buildHelpButton(BuildContext context) {
-    return TextButton.icon(
-      onPressed: () {
-        _showHelpDialog(context);
-      },
-      icon: const Icon(Icons.help_outline,
-          color: Color(0xFFFEDE74)), // Cor amarela para o ícone de ajuda
-      label: Text(
-        'AJUDA',
-        style: GoogleFonts.podkova(
-          fontSize: 16,
-          fontWeight: FontWeight.bold, // Fonte BOLD
-          color: const Color(0xFF37ABDC), // Cor azul para o texto de ajuda
-        ),
-      ),
-    );
-  }
-
-  // Diálogo de ajuda
-  void _showHelpDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Ajuda'),
-          content: const Text(
-              'Aqui você pode ajustar as configurações de som, música, volume e selecionar o idioma da aplicação.'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('Fechar'),
+          color: Colors.white, // Texto sempre branco
+          shadows: [
+            Shadow(
+              color: Colors.black.withOpacity(0.5),
+              offset: const Offset(2, 2),
+              blurRadius: 4,
             ),
           ],
-        );
-      },
+        ),
+      ),
+    );
+  }
+
+  // Função para criar as opções de switch (on/off) com sombra
+  Widget _buildSwitchOption(
+      String title, bool value, ValueChanged<bool> onChanged) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          title,
+          style: GoogleFonts.chewy(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: const Color(0xFFFFFFFF), // Títulos em FCB44E
+            shadows: [
+              Shadow(
+                color: Colors.black.withOpacity(0.5),
+                offset: const Offset(2, 2),
+                blurRadius: 4,
+              ),
+            ],
+          ),
+        ),
+        Switch(
+          value: value,
+          activeColor: const Color(0xFF00D287), // Cor do switch quando ligado
+          inactiveTrackColor:
+              const Color(0xFFF4719C), // Cor do switch quando desligado
+          onChanged: onChanged,
+        ),
+      ],
     );
   }
 }
